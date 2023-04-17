00e0c53c98fd3cd311d4e42acfe6ab866a756570
Patienceineza <99663528+Patienceineza@users.noreply.github.com>
2023-04-17 20:10:29 +0200

    chore(setup): setting up new react project #184948145

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const webpack = require('webpack');
+const Dotenv = require('dotenv-webpack');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env) => ({
+  entry: path.resolve(__dirname, './src/index.js'),
+
+  output: {
+    path: path.join(__dirname, './dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+
+  devServer: {
+    static: path.resolve(__dirname, './dist'),
+    historyApiFallback: true,
+    hot: true,
+  },
+
+  plugins: [
+    env.local
+      ? new Dotenv({
+        path: './.env',
+      })
+      : new Dotenv({
+        systemvars: true,
+      }),
+    new webpack.HotModuleReplacementPlugin(),
+    new HTMLWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.(scss)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.(png|jpeg|jpg|webp|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images/',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+});
