9f960101f8620df2af3d2a9a3fe0996a4c316b8e
blackd44 <irabd44@gmail.com>
2023-04-18 13:01:09 +0200

    chore(sass, readme, redux): - setup sass in the project #184948150 - Setup Redux #184948146 - Create the client README #184948151

---
+++ src/redux/features/slices/sample.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  value: true,
+}
+
+export const booleanSlice = createSlice({
+  name: 'boolean',
+  initialState,
+  reducers: {
+    toggle: (state) => {
+      state.value = !state.value
+    },
+    set: (state, action) => {
+      state.value = action.payload
+    }
+  },
+})
+
+export const { toggle, set } = booleanSlice.actions
+
+export default booleanSlice.reducer
