fefe1944e1bb7e500f8bd35df055fa57efeae8fc
edwinnambaje <nambajeeedwin@gmail.com>
2023-04-18 18:02:14 +0200

    ch(set-up-react-router):implement Setup react router
    -implement Setup react router
    
    [finishes #184948149]

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,19 @@
 /* eslint-disable react/button-has-type */
 import './App.scss';
-import { useSelector, useDispatch } from 'react-redux';
-import { set, toggle } from './redux/features/slices/sample';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Views/Home';
+import Login from './Views/Login';
+import NotFound from './pages/404';
 
 function App() {
-  const bool = useSelector((state) => state.boolean);
-  const dispatch = useDispatch();
-
-  const buttonStyle = {
-    padding: '.5rem 1rem',
-    margin: '5px',
-  };
   return (
-    <div className="App">
-      <p>BROGRAMMERS</p>
-      <div style={{ width: 'max-content', textAlign: 'center' }}>
-        <p>{bool.value ? 'true' : 'false'}</p>
-        <p>
-          <button style={buttonStyle} onClick={() => dispatch(set(true))}>
-            true
-          </button>
-          <button style={buttonStyle} onClick={() => dispatch(toggle())}>
-            inverse
-          </button>
-          <button style={buttonStyle} onClick={() => dispatch(set(false))}>
-            false
-          </button>
-        </p>
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
