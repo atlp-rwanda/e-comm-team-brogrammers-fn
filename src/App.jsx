275d8ec8a67ca66f41f0064dfafcc86f0934c594
edwinnambaje <nambajeeedwin@gmail.com>
2023-04-19 18:53:42 +0200

    ch(set-up-tests):implement Set up tests
    -Implement set up tests on the client
    
    [Finishes #184948148]

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/login" element={<Login/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
