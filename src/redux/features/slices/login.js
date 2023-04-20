2adfa45085445989bd662f90cb9ac115cdd5fda3
blackd44 <irabd44@gmail.com>
2023-04-20 13:42:02 +0200

    adding header drop-down

--- src/redux/features/slices/login.js
+++ src/redux/features/slices/login.js
@@ -1,5 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 import LoginThunk from '../actions/login';
+// eslint-disable-next-line no-unused-vars
 import LogoutThunk from '../actions/logout';
 
 const initialState = {
@@ -13,32 +14,30 @@ export const loginSlice = createSlice({
   name: 'login',
   initialState,
   reducers: {},
-  extraReducers: (builder) => {
-    builder
-      .addCase(LoginThunk.pending, (state) => {
-        state.loading = true;
-        state.error = false;
-      })
-      .addCase(LoginThunk.rejected, (state, { payload }) => {
-        state.loading = false;
+  extraReducers: {
+    [LoginThunk.pending]: (state) => {
+      state.loading = true;
+      state.error = false;
+    },
+    [LoginThunk.rejected]: (state, { payload }) => {
+      state.loading = false;
+      state.error = true;
+      state.errorMessage = { ...payload };
+    },
+    [LoginThunk.fulfilled]: (state, { payload }) => {
+      if (payload.error) {
         state.error = true;
-        state.errorMessage = { ...payload };
-      })
-      .addCase(LoginThunk.fulfilled, (state, { payload }) => {
-        state.loading = false;
-        if (payload.error) {
-          state.error = true;
-          state.errorMessage =
-            payload?.error?.response?.data?.message || 'error';
-        } else if (payload.token) {
-          localStorage.setItem('token', payload.token);
-          state.error = false;
-          state.token = payload.token;
-        }
-      })
-      .addCase(LogoutThunk.fulfilled, (state, { payload }) => {
-        if (payload.status === 200) state.token = null;
-      });
+        state.errorMessage = payload?.error?.response?.data?.message || 'error';
+      } else if (payload.token) {
+        localStorage.setItem('token', payload.token);
+        state.error = false;
+        state.token = payload.token;
+      } else {
+        state.error = true;
+        state.errorMessage = 'unknown error';
+      }
+      state.loading = false;
+    },
   },
 });
 
