7f6f5cb6c5e9a250a5f5a1477f8e306c9dfdbb39
cyubahiro benjamin <bencyubahiro77@gmail.com>
2023-05-08 14:57:34 +0200

    184948169 admin assign role

--- src/Views/admin/User.jsx
+++ src/Views/admin/User.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-else-return */
+/* eslint-disable react/no-array-index-key */
 /* eslint-disable react/button-has-type */
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
@@ -12,12 +14,18 @@ import {
   faTimes,
 } from '@fortawesome/free-solid-svg-icons';
 import { Link, useLocation } from 'react-router-dom';
+import EditUserForm from './EditUserForm';
 
 function AdminDashboard() {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState({
+    data: [],
+    currentPage: 1,
+    rowsPerPage: 13,
+  });
   const [error, setError] = useState('');
   const location = useLocation();
   const [showMenu, setShowMenu] = useState(false);
+  const [editingUser, setEditingUser] = useState(null);
 
   useEffect(() => {
     const token = localStorage.getItem('token');
@@ -29,7 +37,11 @@ function AdminDashboard() {
           },
         })
         .then((response) => {
-          setUsers(response.data.results);
+          setUsers({
+            data: response.data.results,
+            currentPage: 1,
+            rowsPerPage: 13,
+          });
         })
         .catch((err) => {
           setError(err.response.data.message);
@@ -37,8 +49,19 @@ function AdminDashboard() {
     }
   }, []);
 
+  const handlePageChange = (page) => {
+    setUsers({ ...users, currentPage: page });
+  };
+
+  const indexOfLastRow = users.currentPage * users.rowsPerPage;
+  const indexOfFirstRow = indexOfLastRow - users.rowsPerPage;
+  const currentRows = users.data.slice(indexOfFirstRow, indexOfLastRow);
+
   const handleMenuClick = () => {
     setShowMenu(!showMenu);
+  };
+  const handleEditUser = (user) => {
+    setEditingUser(user);
   };
 
   return (
@@ -110,47 +133,108 @@ function AdminDashboard() {
           </Link>
         </div>
         {error && <div className="error">{error}</div>}
-        <div className="table-wrapper">
-          <table className="table">
-            <thead>
-              <tr>
-                <th>Username</th>
-                <th>Email</th>
-                <th>Gender</th>
-                <th>Role</th>
-                <th>Verified</th>
-                <th>Disabled</th>
-                <th className="action">Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              {users.map((user) => (
-                <tr key={user.id}>
-                  <td data-label="Username">{user.username}</td>
-                  <td data-label="Email">{user.email}</td>
-                  <td data-label="Gender">{user.gender}</td>
-                  <td data-label="Role">{user.role}</td>
-                  <td
-                    data-label="Verified"
-                    style={{ color: user.verified ? 'green' : 'red' }}
-                  >
-                    {user.verified ? 'True' : 'False'}
-                  </td>
-                  <td
-                    data-label="Disabled"
-                    style={{ color: user.disabledUser ? 'red' : 'green' }}
-                  >
-                    {user.disabledUser ? 'True' : 'False'}
-                  </td>
-                  <td data-label="Action" className="action">
-                    <button>Edit</button>
-                    <button className="delete">Delete</button>
-                  </td>
+        {editingUser ? (
+          <EditUserForm user={editingUser} />
+        ) : (
+          <div className="table-wrapper">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Username</th>
+                  <th>Email</th>
+                  <th>Gender</th>
+                  <th>Role</th>
+                  <th>Verified</th>
+                  <th>Disabled</th>
+                  <th className="action">Action</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+              </thead>
+              <tbody>
+                {currentRows.map((user) => (
+                  <tr key={user.id}>
+                    <td data-label="Username">{user.username}</td>
+                    <td data-label="Email">{user.email}</td>
+                    <td data-label="Gender">{user.gender}</td>
+                    <td data-label="Role">{user.role}</td>
+                    <td
+                      data-label="Verified"
+                      style={{ color: user.verified ? 'green' : 'red' }}
+                    >
+                      {user.verified ? 'True' : 'False'}
+                    </td>
+                    <td
+                      data-label="Disabled"
+                      style={{ color: user.disabledUser ? 'red' : 'green' }}
+                    >
+                      {user.disabledUser ? 'True' : 'False'}
+                    </td>
+                    <td data-label="Action" className="action">
+                      <button onClick={() => handleEditUser(user)}>Edit</button>
+                      <button className="delete">Delete</button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <div className="pagination">
+              {users.currentPage > 1 && (
+                <button onClick={() => handlePageChange(users.currentPage - 1)}>
+                  Prev
+                </button>
+              )}
+              {Array(Math.ceil(users.data.length / users.rowsPerPage))
+                .fill()
+                .map((_, index) => {
+                  const minPage = Math.max(users.currentPage - 1, 1);
+                  const maxPage = Math.min(
+                    users.currentPage + 1,
+                    Math.ceil(users.data.length / users.rowsPerPage)
+                  );
+                  const showLeftEllipsis = minPage > 1;
+                  const showRightEllipsis =
+                    maxPage < Math.ceil(users.data.length / users.rowsPerPage);
+                  if (
+                    index + 1 === 1 ||
+                    index + 1 ===
+                      Math.ceil(users.data.length / users.rowsPerPage) ||
+                    (index + 1 >= minPage && index + 1 <= maxPage)
+                  ) {
+                    return (
+                      <button
+                        key={index}
+                        onClick={() => handlePageChange(index + 1)}
+                        className={
+                          users.currentPage === index + 1 ? 'active' : ''
+                        }
+                        style={{ marginLeft: '5px', marginRight: '5px' }}
+                      >
+                        {index + 1}
+                      </button>
+                    );
+                  } else if (
+                    (index + 1 === minPage - 1 && showLeftEllipsis) ||
+                    (index + 1 === maxPage + 1 && showRightEllipsis)
+                  ) {
+                    return (
+                      <span
+                        key={index}
+                        style={{ marginLeft: '5px', marginRight: '5px' }}
+                      >
+                        ...
+                      </span>
+                    );
+                  }
+                  return null;
+                })}
+              {users.currentPage <
+                Math.ceil(users.data.length / users.rowsPerPage) && (
+                <button onClick={() => handlePageChange(users.currentPage + 1)}>
+                  Next
+                </button>
+              )}
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
